88a5e334db016fdc946a871fb047fdfaf8ae11e4
emenesism <mhdnris@gmail.com>
2024-09-12 02:01:57 +0330

    register the controller in the service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './repositories/user.repository';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UserRepositoryResponse } from './interfaces/repositoryResponse';
 
 @Injectable()
-export class UsersService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async createUser(
+    createUserData: CreateUserDTO,
+  ): Promise<UserRepositoryResponse> {
+    return this.userRepository.createUser(createUserData);
+  }
+
+  async getUserById(id: string): Promise<UserRepositoryResponse> {
+    return this.userRepository.getUserById(id);
+  }
+
+  async getAllUsers(): Promise<UserRepositoryResponse> {
+    return this.userRepository.getAllUsers();
+  }
+
+  async getUserByEmail(email: string): Promise<UserRepositoryResponse> {
+    return this.userRepository.getUserByEmail(email);
+  }
+
+  async updateUser(
+    id: string,
+    updateData: Partial<CreateUserDTO>,
+  ): Promise<UserRepositoryResponse> {
+    return this.userRepository.updateUser(id, updateData);
+  }
+}
