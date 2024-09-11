b6c8fd4cc4972b0d81699858c8052abb889c07a3
emenesism <mhdnris@gmail.com>
2024-09-11 17:15:19 +0330

    set up project structure and add initial modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { TodoModule } from './modules/todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, AuthModule, UsersModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/todo/todo.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
+import { TodoController } from './todo.controller';
+import { TodoController } from './todo.controller';
+
+@Module({
+  providers: [TodoService],
+  controllers: [TodoController]
+})
+export class TodoModule {}
