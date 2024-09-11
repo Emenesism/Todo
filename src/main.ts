81ca1d511ae841116b6917ed0ad76e4f59757e58
emenesism <mhdnris@gmail.com>
2024-09-12 02:00:48 +0330

    add global pipeline for validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service'; // Ensure correct path
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get<ConfigService>(ConfigService);
 
