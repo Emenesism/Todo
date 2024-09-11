81de24fae2d4ee2695804e38a5e4288f85a32900
emenesism <mhdnris@gmail.com>
2024-09-12 02:35:28 +0330

    import and export service

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { BcryptUtils } from 'src/common/utils/password.service';
+import { UserRepository } from './repositories/user.repository';
+import { AppLoggerService } from 'src/common/utils/logger.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, BcryptUtils, UserRepository, AppLoggerService],
+  exports: [UsersService, UserRepository],
 })
 export class UsersModule {}
