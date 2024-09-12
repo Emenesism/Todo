534cd998fbc347fd307c2a343f78b93d812897fe
emenesism <mhdnris@gmail.com>
2024-09-12 23:03:23 +0330

    update the todo module

--- src/modules/todo/todo.module.ts
+++ src/modules/todo/todo.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
+import { AppLoggerService } from 'src/common/utils/logger.service';
+import { TodoRepository } from './repositories/todo.repository';
 import { TodoController } from './todo.controller';
+import { AuthModule } from '../auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [TodoService],
+  imports: [AuthModule, JwtModule],
+  providers: [TodoService, AppLoggerService, TodoRepository],
   controllers: [TodoController],
 })
 export class TodoModule {}
