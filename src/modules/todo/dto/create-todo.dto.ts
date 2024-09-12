53caff90820bbff92a8fa5fac4e66ca27a36ae61
emenesism <mhdnris@gmail.com>
2024-09-12 23:45:36 +0330

    delete some body part that is not needed

--- src/modules/todo/dto/create-todo.dto.ts
+++ src/modules/todo/dto/create-todo.dto.ts
@@ -1,4 +1,4 @@
-import { IsBoolean, IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTodoDTO {
   @IsNotEmpty({ message: 'Title should not be empty' })
@@ -9,15 +9,9 @@ export class CreateTodoDTO {
   @IsString({ message: 'Description should be string' })
   description: string;
 
-  @IsNotEmpty({ message: 'IsDone should not be empty' })
-  @IsBoolean({ message: 'IsDone should be boolean' })
-  isDone: boolean;
-
   @IsNotEmpty({ message: 'Until should not be empty' })
-  @IsDate({ message: 'Until should be date Format' })
+  @IsDateString({}, { message: 'Until should be date Format' })
   until: Date;
 
-  @IsNotEmpty({ message: 'UserGmail should not be empty' })
-  @IsString({ message: 'UserId should be string' })
   userId: string;
 }
