8c8dff158cf9dfbe45900bb126c3b6db07a89cec
emenesism <mhdnris@gmail.com>
2024-09-12 18:58:44 +0330

    correct the type of response

--- src/modules/users/interfaces/repositoryResponse.ts
+++ src/modules/users/interfaces/repositoryResponse.ts
@@ -1,14 +1,5 @@
-export interface UserRepositoryResponseOnSuccessTrue {
+export interface UserRepositoryResponse {
   success: boolean;
   message: string;
-  data: any;
+  data?: any;
 }
-
-export interface UserRepositoryResponseOnSuccessFalse {
-  success: boolean;
-  message: string;
-}
-
-export type UserRepositoryResponse =
-  | UserRepositoryResponseOnSuccessFalse
-  | UserRepositoryResponseOnSuccessTrue;
